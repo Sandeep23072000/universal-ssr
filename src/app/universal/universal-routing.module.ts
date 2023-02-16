229aa22aad8694cc1daca59b415a78ae4440aa7e
sandeep23072000 <ssanini70@gmail.com>
2023-02-16 11:52:59 +0530

    commit

--- src/app/universal/universal-routing.module.ts
+++ src/app/universal/universal-routing.module.ts
@@ -6,11 +6,12 @@ import { LoginComponent } from './login/login.component';
 import { UniversalComponent } from './universal.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent,data: {title:"home page is here",discription:"get the home page here  for showing data",ogTitle: 'Description of Home Component for social media',}},
+  { path: '', component: HomeComponent,data: {title:"Angular Universal home page is here for use the SEO",discription:
+  "Angular Universal executes on the server, generating static application pages that later get bootstrapped on the client. This means that the application generally renders more quickly, giving users a chance to view the application layout before it becomes fully interactive",ogTitle: 'Description of Home Component for social media',}},
 
-  { path: 'login', component: LoginComponent,data: {title:"login page is here",discription:"get the login page here  for showing data",ogTitle: 'Description of Login Component for social media',}},
+  { path: 'login', component: LoginComponent,data: {title:"Angular Universal login page is here for use the SEO",discription:"get the login page here  for showing data",ogTitle: 'Description of Login Component for social media',}},
 
-  {path: 'details', component: DetailsComponent,data: {title:"details page is here",discription:"get the details page here  for showing data",ogTitle: 'Description of Login Component for social media',}},
+  {path: 'details', component: DetailsComponent,data: {title:"Angular Universal details page is here for use the SEO",discription:"get the details page here  for showing data",ogTitle: 'Description of Login Component for social media',}},
 ];
 
 @NgModule({

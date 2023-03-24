1d07865ad3aa98406bc24258625353d42acfa131
sandeep23072000 <ssanini70@gmail.com>
2023-03-24 11:03:39 +0530

    commit

--- src/app/universal/details/details.component.ts
+++ src/app/universal/details/details.component.ts
@@ -11,11 +11,12 @@ import { CanonicalService } from 'src/app/services/canonical.service';
 })
 export class DetailsComponent implements OnInit {
   productlist: any;
+  iddata: any;
   images: any;
 
   constructor(private meta: Meta, private title: Title, private route: ActivatedRoute, private http: HttpClient, private canService: CanonicalService) {
-    const iddata = this.route.snapshot.params['id'];
-    this.http.get('https://dummyjson.com/products/' + iddata).subscribe((response: any) => {
+    this.iddata = this.route.snapshot.params['id'];
+    this.http.get('https://dummyjson.com/products/' + this.iddata).subscribe((response: any) => {
       console.log(response)
       this.productlist = response;
     })
@@ -28,16 +29,16 @@ export class DetailsComponent implements OnInit {
     
     this.meta.updateTag({ property: "og:title", content: "Product Details page is here" });
     this.meta.updateTag({ property: "og:type", content: "website" });
-    this.meta.updateTag({ property: "og:image", content: "https://picsum.photos/200/300?random=2" });
+    this.meta.updateTag({ property: "og:image", content: "https://picsum.photos/200/300" });
     this.meta.updateTag({ property: "og:image:alt", content: "Og Image" });
-    this.meta.updateTag({ property: "og:url", content: "http//:localhost:4200/details/id" });
+    this.meta.updateTag({ property: "og:url", content:"http//:localhost:4200/details/" + this.iddata });
     this.meta.updateTag({ property: "og:description", content: "Since some online shoppers only scan text on websites, it might be helpful to use bullet points that cover the most important product details. Bullet points should generally be used for specs or short phrases  so they are quick and easy to read." });
 
 
     this.meta.updateTag({ name: "twitter:card", content: "summary" });
     this.meta.updateTag({ name: "twitter:site", content: "@getproducts" });
     this.meta.updateTag({ name: "twitter:creator", content: "@productsh" });
-    this.meta.addTag({ name: "twitter:image", content: "https://picsum.photos/200/300?random=2" });
+    this.meta.addTag({ name: "twitter:image", content: "https://picsum.photos/200/300" });
     this.meta.addTag({ name: "twitter:image:alt", content: "Images" });
     this.meta.updateTag({ name: "twitter:title", content: "Details of Products" });
     this.meta.updateTag({ name: "twitter:description", content: "Here, We will discribe the list of related products as Mobile phones, laptops and other type of electronic products and some more." });

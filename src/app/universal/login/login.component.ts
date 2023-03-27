8497d2892c5277bfe594328251fa24e12946e044
sandeep23072000 <ssanini70@gmail.com>
2023-03-27 13:38:18 +0530

    commit15

--- src/app/universal/login/login.component.ts
+++ src/app/universal/login/login.component.ts
@@ -20,14 +20,21 @@ export class LoginComponent implements OnInit {
     console.log(this.canService.setCanonicalURL);
     // this.title.setTitle('login here for universal access');
 
-    this.meta.addTags([
-      { property: 'og:title', content: 'My Angular Universal Application' },
-      { property: "og:image", content: "https://picsum.photos/200/300?random=2" },
-      { property: "og:image:alt", content: "Og Image" },
-      { property: 'og:description', content: 'This is my Angular Universal application' },
-      { property: "og:url", content: "https://universal-ssr.netlify.app/login" }
+    this.meta.addTag({ property: "og:title", content: "Login page for products" });
+    this.meta.addTag({ property: "og:type", content: "Here, We can see product list according to desired products." });
+    this.meta.addTag({ property: "og:image", content: "https://picsum.photos/200/300?random=2" });
+    this.meta.addTag({ property: "og:image:alt", content: "Og Image" });
+    this.meta.addTag({ property: "og:url", content: "https://universal-ssr.netlify.app/login" });
+    this.meta.addTag({ property: "og:description", content:"this is login page for products and services"});
 
-    ])
+    // this.meta.addTags([
+    //   { property: 'og:title', content: 'My Angular Universal Application' },
+    //   { property: "og:image", content: "https://picsum.photos/200/300?random=2" },
+    //   { property: "og:image:alt", content: "Og Image" },
+    //   { property: 'og:description', content: 'This is my Angular Universal application' },
+    //   { property: "og:url", content: "https://universal-ssr.netlify.app/login" }
+
+    // ])
     this.meta.addTag({ name: "twitter:card", content: "summary" });
     this.meta.addTag({ name: "twitter:site", content: "@getproducts" });
     this.meta.addTag({ name: "twitter:creator", content: "@productlist" });

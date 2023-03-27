cb91fb6923bb60455753ae7d663e5e0124b02dc4
sandeep23072000 <ssanini70@gmail.com>
2023-03-27 14:24:39 +0530

    commit17

--- src/app/universal/details/details.component.ts
+++ src/app/universal/details/details.component.ts
@@ -31,7 +31,7 @@ export class DetailsComponent implements OnInit {
     this.meta.addTag({ property: "og:type", content: "Here, We can see product datails according to desired products." });
     this.meta.addTag({ property: "og:image", content: "https://picsum.photos/200/300?random=2" });
     this.meta.addTag({ property: "og:image:alt", content: "Og Image" });
-    this.meta.addTag({ property: "og:url", content: `https://universal-ssr.netlify.app/details + ${this.iddata}` });
+    this.meta.addTag({ property: "og:url", content: "https://universal-ssr.netlify.app/details" });
     this.meta.updateTag({ property: "og:description", content: "Since some online shoppers only scan text on websites, it might be helpful to use bullet points that cover the most important product details. Bullet points should generally be used for specs or short phrases  so they are quick and easy to read." });
 
 

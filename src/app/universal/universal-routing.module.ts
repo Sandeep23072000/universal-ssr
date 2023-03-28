750dd36c517f0656b24905319bbcd71cf8ddbed1
sandeep23072000 <ssanini70@gmail.com>
2023-03-28 11:44:35 +0530

    commit5

--- src/app/universal/universal-routing.module.ts
+++ src/app/universal/universal-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
 
   { path: 'login', component: LoginComponent},
 
-  {path: 'details/:id', component: DetailsComponent},
+  {path: 'productdetails/:id', component: DetailsComponent},
 ];
 
 @NgModule({

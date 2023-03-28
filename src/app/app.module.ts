e9536062c8d2a2c727f08647874501df350b2c20
sandeep23072000 <ssanini70@gmail.com>
2023-03-28 17:55:44 +0530

    commit28

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@a
     RouterModule,
      
   ],
-  providers: [{provide: LocationStrategy, useClass: PathLocationStrategy}],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

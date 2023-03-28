78cdf3448d70c8c60c33b2499cafa47e82e741a3
sandeep23072000 <ssanini70@gmail.com>
2023-03-28 16:49:18 +0530

    commit21

--- src/app/universal/login/login.component.ts
+++ src/app/universal/login/login.component.ts
@@ -24,7 +24,7 @@ export class LoginComponent implements OnInit {
     this.meta.updateTag({ property: "og:type", content: "Here, We can see product list according to desired products." });
     this.meta.updateTag({ property: "og:image", content: "https://picsum.photos/200/300?random=2" });
     this.meta.updateTag({ property: "og:image:alt", content: "Og Image" });
-    this.meta.updateTag({ property: "og:url", content: "https://universal-ssr.netlify.app/login" });
+    this.meta.updateTag({ property: "og:url", content: "https://universal-ssr.netlify.app/#/login" });
     this.meta.updateTag({ property: "og:description", content:"this is login page for products and services"});
 
     // this.meta.addTags([

712464b23e2eaa48e770e7ca124b160e5ffe505b
sandeep23072000 <ssanini70@gmail.com>
2023-03-28 11:30:27 +0530

    commit3

--- src/app/universal/login/login.component.ts
+++ src/app/universal/login/login.component.ts
@@ -20,12 +20,12 @@ export class LoginComponent implements OnInit {
     // console.log(this.canService.setCanonicalURL);
     // this.title.setTitle('login here for universal access');
 
-    this.meta.addTag({ property: "og:title", content: "Login page for products" });
-    this.meta.addTag({ property: "og:type", content: "Here, We can see product list according to desired products." });
-    this.meta.addTag({ property: "og:image", content: "https://picsum.photos/200/300?random=2" });
-    this.meta.addTag({ property: "og:image:alt", content: "Og Image" });
-    this.meta.addTag({ property: "og:url", content: "https://universal-ssr.netlify.app/login" });
-    this.meta.addTag({ property: "og:description", content:"this is login page for products and services"});
+    this.meta.updateTag({ property: "og:title", content: "Login page for products" });
+    this.meta.updateTag({ property: "og:type", content: "Here, We can see product list according to desired products." });
+    this.meta.updateTag({ property: "og:image", content: "https://picsum.photos/200/300?random=2" });
+    this.meta.updateTag({ property: "og:image:alt", content: "Og Image" });
+    this.meta.updateTag({ property: "og:url", content: "https://universal-ssr.netlify.app/login" });
+    this.meta.updateTag({ property: "og:description", content:"this is login page for products and services"});
 
     // this.meta.addTags([
     //   { property: 'og:title', content: 'My Angular Universal Application' },
@@ -35,11 +35,11 @@ export class LoginComponent implements OnInit {
     //   { property: "og:url", content: "https://universal-ssr.netlify.app/login" }
 
     // ])
-    this.meta.addTag({ name: "twitter:card", content: "summary" });
-    this.meta.addTag({ name: "twitter:site", content: "@getproducts" });
-    this.meta.addTag({ name: "twitter:creator", content: "@productlist" });
-    this.meta.addTag({ name: "twitter:title", content: "Login for better experience" });
-    this.meta.addTag({ name: "twitter:description", content: "Contactus- getblog" });
+    this.meta.updateTag({ name: "twitter:card", content: "summary" });
+    this.meta.updateTag({ name: "twitter:site", content: "@getproducts" });
+    this.meta.updateTag({ name: "twitter:creator", content: "@productlist" });
+    this.meta.updateTag({ name: "twitter:title", content: "Login for better experience" });
+    this.meta.updateTag({ name: "twitter:description", content: "Contactus- getblog" });
   }
 
 }
